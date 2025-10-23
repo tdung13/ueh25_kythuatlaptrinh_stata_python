b9123c276ae2f6de30e6fd4f66d597576569c63b
tdung13 <dnguyenthuy831@gmail.com>
2025-10-23 22:45:30 +0700

    Delete buoi02_250819 directory

--- buoi02_250819/first.cpp
+++
@@ -1,7 +0,0 @@
-#include 
-int main(void) {
-  int a=27, b=6, c;
-  c = a%b;
-  printf("The value of c is %d.\n", c);
-  return 0;
-}
--- buoi02_250819/first.py
+++
@@ -1,4 +0,0 @@
-a = 27
-b = 6
-c = a%b;
-print("The value of c is %d.\n"% c)
